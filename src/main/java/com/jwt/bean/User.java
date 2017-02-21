795980dbd648051e796f9a45e70f726fcb021838
Tabrez Alam <metabrez@gmail.com>
2017-02-20 21:14:32 -0600

    This is first

---
+++ src/main/java/com/jwt/bean/User.java
@@ -0,0 +1,47 @@
+package com.jwt.bean;
+
+public class User {
+
+	private String name;
+	private String email;
+	private int age;
+	private String sex = null;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", Email=" + email + ", age=" + age
+				+ ", sex=" + sex + "]";
+	}
+}
